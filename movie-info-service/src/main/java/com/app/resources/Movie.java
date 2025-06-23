65a5c2190af5ff561e1e558c27af9fe7102e9ef0
vinaykumar-zine <vkumarzine@gmail.com>
2025-06-23 10:50:55 +0530

    base version movie-info-service

---
+++ movie-info-service/src/main/java/com/app/resources/Movie.java
@@ -0,0 +1,13 @@
+package com.app.resources;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Movie {
+	private String movieId;
+	private String name;
+}
