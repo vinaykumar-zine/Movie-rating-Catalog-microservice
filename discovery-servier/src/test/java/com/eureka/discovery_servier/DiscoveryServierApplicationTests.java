9462ef8ed2df3d485c11fcc71de9632d2af43024
vinaykumar-zine <vkumarzine@gmail.com>
2025-06-23 10:55:24 +0530

    base version discovery-server

---
+++ discovery-servier/src/test/java/com/eureka/discovery_servier/DiscoveryServierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.discovery_servier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
